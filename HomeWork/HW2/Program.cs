e4436f7326d254a7518e4f7d47486309e840844d
Ilya Bozhkov <ilia.il.27@mail.ru>
2022-02-24 19:27:54 +0300

    Completed task 1

--- HomeWork/HW2/Program.cs
+++ HomeWork/HW2/Program.cs
@@ -6,7 +6,22 @@ namespace HW2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Task 1");
+            ArithmeticOperations();
+        }
+
+        static void ArithmeticOperations()
+        {
+            Console.Write("Enter the first number: ");
+            double number1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter the second number: ");
+            double number2 = Convert.ToDouble(Console.ReadLine());
+            if (number1 > number2)
+                Console.WriteLine($"Let's add these two numbers: A + B = {number1 + number2}");
+            else if (number1 < number2)
+                Console.WriteLine($"Subtract the second from the first number: A - B = {number1 - number2}");
+            else
+                Console.WriteLine($"Multiply these two numbers: A * B = {number1 * number2}");
         }
     }
 }
