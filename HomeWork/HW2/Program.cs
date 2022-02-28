05b1e92dea42d2c8efa96aea247728b564362fa6
IlyaBozh <86633291+IlyaBozh@users.noreply.github.com>
2022-02-28 22:30:19 +0300

    Completed HW 2

--- HomeWork/HW2/Program.cs
+++ HomeWork/HW2/Program.cs
@@ -30,14 +30,19 @@ namespace HW2
             double number2 = Convert.ToDouble(Console.ReadLine());
 
             if (number1 > number2)
+            {
                 Console.WriteLine($"Let's add these two numbers: A + B = {number1 + number2}");
-
+            }
+            
             else if (number1 < number2)
+            {
                 Console.WriteLine($"Subtract the second from the first number: A - B = {number1 - number2}");
-
+            }
+            
             else
+            {
                 Console.WriteLine($"Multiply these two numbers: A * B = {number1 * number2}");
-
+            }
         }
 
         static void DefinitionQuarterCoordinatePlane()
@@ -49,25 +54,39 @@ namespace HW2
             double y = Convert.ToDouble(Console.ReadLine());
 
             if (x == 0 && y == 0)
+            {
                 Console.WriteLine("The point lies at the origin of the coordinates");
-
+            }
+            
             else if (x == 0)
+            {
                 Console.WriteLine("The point lies on the x axis");
-
+            }
+            
             else if (y == 0)
+            {
                 Console.WriteLine("The point lies on the y axis");
-
+            }
+            
             else if (x > 0 && y > 0)
+            {
                 Console.WriteLine("The point belongs to the first quarter of the coordinate plane");
-
+            }
+            
             else if (x < 0 && y > 0)
+            {
                 Console.WriteLine("The point belongs to the second quarter of the coordinate plane");
-
+            }
+            
             else if (x < 0 && y < 0)
+            {
                 Console.WriteLine("The point belongs to the third quarter of the coordinate plane");
-
+            }
+            
             else
+            {
                 Console.WriteLine("The point belongs to the fourth quarter of the coordinate plane");
+            }
         }
 
         static void OutputAscendingOrder()
@@ -80,22 +99,34 @@ namespace HW2
             double number3 = Convert.ToDouble(Console.ReadLine());
 
             if (number1 > number2 && number2 > number3)
+            {
                 Console.WriteLine($"Numbers in ascending order: {number3} {number2} {number1}");
-
+            }
+            
             else if (number1 > number2 && number3 > number2)
+            {
                 Console.WriteLine($"Numbers in ascending order: {number2} {number3} {number1}");
-
+            }
+            
             else if (number2 > number1 && number1 > number3)
+            {
                 Console.WriteLine($"Numbers in ascending order: {number3} {number1} {number2}");
-
+            }
+            
             else if (number2 > number1 && number3 > number1)
+            {
                 Console.WriteLine($"Numbers in ascending order: {number1} {number3} {number2}");
-
+            }
+            
             else if (number3 > number1 && number2 > number1)
+            {
                 Console.WriteLine($"Numbers in ascending order: {number1} {number2} {number3}");
-
+            }
+            
             else
+            {
                 Console.WriteLine($"Numbers in ascending order: {number2} {number1} {number3}");
+            }
         }
 
         static void SolvingQuadraticEquation()
